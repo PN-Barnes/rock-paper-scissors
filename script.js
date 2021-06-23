71be3502972e9b8109fd60378ad4ad013dfdd6ca
PN-Barnes <paulieb99@gmail.com>
2021-06-23 11:30:57 -0700

    Wrote functions to check inputs against eachother

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-var userChoice = prompt("Please select R, P, or S.");
+
 
 var playAgain = confirm("Do you want to play again?")
 
-var scoreBoard = alert("")
-console.log(userChoice);
+var scoreBoard = alert("");
+
+var computerOptions = ["R", "S", "P"];
+
+var wins = 0;
+var tie = 0;
+var losses = 0;
+
+function promptUserInput() {
+    var userPrompt = prompt("Please select R, P, or S.");
+    return userPrompt;
+}
+
+function randomGenerator() {
+    var numIndex = Math.floor(Math.random() * 3);
+    var computerChoice = computerOptions[numIndex];
+    return computerChoice;
+}
+
+function checkUserInput(input) {
+    return input === computerOptions;
+}
+
+// var userChoice = userPrompt.toUpperCase();
+
+function playGame() {
+    var userChoice = promptUserInput();
+    var computerChoice = randomGenerator();
+    console.log(userChoice);
+    console.log(computerChoice);
+    compareInputs(userChoice, computerChoice); 
+}
+
+function compareInputs(userChoice, computerChoice) {
+    // Compare the user choice against the computer choice. 
+}
+
+playGame();
