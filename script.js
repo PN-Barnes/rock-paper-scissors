87f91c4da8e53737b788ce2938b9a46515d9b255
PN-Barnes <paulieb99@gmail.com>
2021-06-23 12:20:01 -0700

    finished code for the game

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 
-
-var playAgain = confirm("Do you want to play again?")
-
-var scoreBoard = alert("");
-
 var computerOptions = ["R", "S", "P"];
 
 var wins = 0;
@@ -12,11 +7,12 @@ var losses = 0;
 
 function promptUserInput() {
     var userPrompt = prompt("Please select R, P, or S.");
-    return userPrompt;
+    var fixPrompt = userPrompt.toUpperCase();
+    return fixPrompt;
 }
 
 function randomGenerator() {
-    var numIndex = Math.floor(Math.random() * 3);
+    var numIndex = Math.floor(Math.random() * computerOptions.length);
     var computerChoice = computerOptions[numIndex];
     return computerChoice;
 }
@@ -35,8 +31,43 @@ function playGame() {
     compareInputs(userChoice, computerChoice); 
 }
 
-function compareInputs(userChoice, computerChoice) {
+function compareInputs(user, computer) {
     // Compare the user choice against the computer choice. 
+    var winner = ""
+    if(user === computer) {
+        alert('You tied!');
+        tie++;
+    } else if(user === computerOptions[0] && computer === computerOptions[1]) {
+        alert('You won!');
+        wins++;
+    } else if(user === computerOptions[0] && computer === computerOptions[2]) {
+        alert('You lost!');
+        losses++;
+    } else if(user === computerOptions[1] && computer === computerOptions[0]) {
+        alert('You lost!');
+        losses++;
+    } else if(user === computerOptions[1] && computer === computerOptions[2]) {
+        alert('You won!');
+        wins++;
+    } else if(user === computerOptions[2] && computer === computerOptions[0]) {
+        alert('You won!');
+        wins++;
+    } else if(user === computerOptions[2] && computer === computerOptions[1]) {
+        alert('You lost!');
+        losses++
+    } else {
+        alert("Did You select R, P, or S?")
+    }
+    return 
 }
 
 playGame();
+
+var scoreBoard = alert(`Wins: ${wins}  losses: ${losses}  ties: ${tie}`);
+
+var playAgain = confirm("Do you want to play again?")
+
+
+if(playAgain) {
+    playGame();
+}
